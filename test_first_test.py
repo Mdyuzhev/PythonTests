34edb1842000e387e2d1c5258e1aca0c88c37e3b
Mdyuzhev <42672538+Mdyuzhev@users.noreply.github.com>
2019-05-11 23:54:28 +0300

    add contact

--- test_first_test.py
+++ test_first_test.py
@@ -11,24 +11,45 @@ class AppDynamicsJob(unittest.TestCase):
 
     def test_app_dynamics_job(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("add new").click()
+        self.open_home_page(driver)
+        self.login(driver, "admin", "secret")
+        self.add_new_contact(driver)
+        self.fill_contact_form(driver, "first", "middle")
+        self.submit_contact(driver)
+        self.go_to_home_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def go_to_home_page(self, driver):
+        driver.find_element_by_link_text("home page").click()
+
+    def submit_contact(self, driver):
+        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def fill_contact_form(self, driver, firstname, middlename):
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("first")
+        driver.find_element_by_name("firstname").send_keys(firstname)
         driver.find_element_by_name("middlename").click()
         driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys("middle")
-        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        driver.find_element_by_link_text("home page").click()
-        driver.find_element_by_link_text("Logout").click()
+        driver.find_element_by_name("middlename").send_keys(middlename)
+
+    def add_new_contact(self, driver):
+        driver.find_element_by_link_text("add new").click()
+
+    def login(self, driver, username, password):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.driver.quit()
