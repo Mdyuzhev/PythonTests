2735925d8bb781bda0a1b506ad77b20f419295be
Mdyuzhev <42672538+Mdyuzhev@users.noreply.github.com>
2019-05-12 14:45:57 +0300

    create group helper

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
+from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 
 
@@ -8,41 +9,11 @@ class Application:
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def goTo_home(self):
         driver = self.driver
         driver.find_element_by_link_text("home").click()
-
-    def back_to_group_page(self):
-        driver = self.driver
-
-        driver.find_element_by_link_text("group page").click()
-
-    def submit_new_group(self):
-        driver = self.driver
-        driver.find_element_by_name("submit").click()
-
-    def fill_group_form(self, group):
-        driver = self.driver
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-
-    def add_new_group(self):
-        driver = self.driver
-        driver.find_element_by_name("new").click()
-
-    def goTo_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
-
-
 
 
 
---
+++ fixture/group.py
@@ -0,0 +1,31 @@
+class GroupHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def goTo_groups_page(self):
+        driver = self.app.driver
+        driver.find_element_by_link_text("groups").click()
+
+    def back_to_group_page(self):
+        driver = self.app.driver
+        driver.find_element_by_link_text("group page").click()
+
+    def submit_new_group(self):
+        driver = self.app.driver
+        driver.find_element_by_name("submit").click()
+
+    def fill_group_form(self, group):
+        driver = self.app.driver
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def add_new_group(self):
+        driver = self.app.driver
+        driver.find_element_by_name("new").click()
