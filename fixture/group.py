f48fb90bb7b4ce38e03c8be012897a3cc88f63d6
Mdyuzhev <42672538+Mdyuzhev@users.noreply.github.com>
2019-05-13 00:10:06 +0300

    test modification

--- fixture/group.py
+++ fixture/group.py
@@ -14,24 +14,37 @@ class GroupHelper:
         driver = self.app.driver
         driver.find_element_by_name("submit").click()
 
+    def update_group(self):
+        driver = self.app.driver
+        driver.find_element_by_name("update").click()
+
     def fill_group_form(self, group):
         driver = self.app.driver
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value(group, "group_name", group.header)
+        self.change_field_value(group, "group_header", group.header)
+        self.change_field_value(group, "group_footer", group.footer)
 
-    def delete_first_group(self):
+    def change_field_value(self, group, field_name, text):
+        driver = self.app.driver
+        if text is not None:
+            driver.find_element_by_name(field_name).click()
+            driver.find_element_by_name(field_name).clear()
+            driver.find_element_by_name(field_name).send_keys(text)
+
+    def select_first_group(self):
         driver = self.app.driver
         driver.find_element_by_name("selected[]").click()
-        driver.find_element_by_xpath("(//input[@name='delete'])[2]").click()
-
 
     def add_new_group(self):
         driver = self.app.driver
         driver.find_element_by_name("new").click()
+
+    def delete_first_group(self):
+        driver = self.app.driver
+        self.select_first_group()
+        driver.find_element_by_xpath("(//input[@name='delete'])[2]").click()
+
+    def modify_first_group(self):
+        driver = self.app.driver
+        self.select_first_group()
+        driver.find_element_by_name("edit").click()
